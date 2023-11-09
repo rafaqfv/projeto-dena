a7d61a2ac45ac4a5353b2d2d333d4b7a2ae2de11
Rafael <127614067+rafaqfv@users.noreply.github.com>
2023-11-09 16:00:45 -0300

    adicionei o <dialog>

--- scripts/favoritos.js
+++ scripts/favoritos.js
@@ -4,10 +4,10 @@ fav.forEach((button) => {
   button.addEventListener("click", (event) => {
     const clickedButton = event.target; // Obtém o botão específico que foi clicado
 
-    if (clickedButton.innerText === "heart_plus") {
+    if (clickedButton.innerText === "favorite_border") {
       clickedButton.innerText = "favorite";
     } else {
-      clickedButton.innerText = "heart_plus";
+      clickedButton.innerText = "favorite_border";
     }
   });
 });
