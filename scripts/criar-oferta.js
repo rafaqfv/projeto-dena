7b7005e59fdd36915fb43a1746030753bf1a790e
Rafael <127614067+rafaqfv@users.noreply.github.com>
2023-11-23 20:56:08 -0300

    Tentando corrigir pesquisa 2

--- scripts/criar-oferta.js
+++ scripts/criar-oferta.js
@@ -197,3 +197,28 @@ function docAdd(item) {
       console.error("Erro ao adicionar documento: ", error);
     });
 }
+
+function uploadImg() {
+
+}
+
+
+
+// ! Pesquisa de items
+const inputSearch = document.getElementById("inputSearch");
+inputSearch.addEventListener("input", searchItems);
+
+function searchItems() {
+  const searchTerm = inputSearch.value.toLowerCase();
+
+  const allItems = document.querySelectorAll(".product-item");
+
+  allItems.forEach((item) => {
+    const title = item.querySelector("h2").textContent.toLowerCase();
+    if (title.includes(searchTerm)) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
