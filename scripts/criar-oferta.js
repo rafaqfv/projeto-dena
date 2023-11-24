ce9e983c78d3582b28da274093e8f5cf49077a16
Rafael <127614067+rafaqfv@users.noreply.github.com>
2023-11-23 22:57:20 -0300

    Refatorando

--- scripts/criar-oferta.js
+++ scripts/criar-oferta.js
@@ -15,6 +15,24 @@ addItemModal.addEventListener("close", resetForm);
 addItemModal.addEventListener("submit", handleSubmit);
 addItemModal.addEventListener("reset", closeAndResetForm);
 window.addEventListener("DOMContentLoaded", loadProducts);
+document.addEventListener("DOMContentLoaded", function () {
+  const imgItemInput = document.getElementById("imgItem");
+  const previewImage = document.getElementById("previewImage");
+
+  imgItemInput.addEventListener("change", function () {
+    const file = imgItemInput.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        previewImage.src = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  });
+});
 
 // Funções
 function openModal() {
